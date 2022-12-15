1b372151a8c33889e9f22a440df408ab2cd43367
Jovan Kostic <kosticjovan1@gmail.com>
2022-12-15 13:46:53 +0100

    Kraj

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-z1 = new Audio()
-z2 = new Audio()
-z3 = new Audio()
-z4 = new Audio()
-z5 = new Audio()
+let z1 = new Audio("./zvuci/pas.wav")
+let z2 = new Audio("./zvuci/patka.mp3")
+let z3 = new Audio("./zvuci/macka.mp3")
+let z4 = new Audio("./zvuci/pile.wav")
+let z5 = new Audio("./zvuci/krava.wav")
 
 
 document.getElementById('z1').addEventListener("mouseover", ()=>{
@@ -21,3 +21,37 @@ document.getElementById('z5').addEventListener("mouseover", ()=>{
     z5.play();
 })
 
+let stranica = "zivotinje";
+let dugme = "pocetna";
+
+
+document.getElementById('pocetna').addEventListener("click", ()=>{
+    document.getElementById(stranica).classList.add("skriveno")
+    stranica = "zivotinje";
+    document.getElementById('zivotinje').classList.remove("skriveno");
+
+    document.getElementById(dugme).classList.remove("trenutno");
+    dugme = "pocetna";
+    document.getElementById('pocetna').classList.add("trenutno");
+
+})
+
+document.getElementById('autordugme').addEventListener("click", ()=>{
+    document.getElementById(stranica).classList.add("skriveno")
+    stranica = "autor";
+    document.getElementById('autor').classList.remove("skriveno");
+    
+    document.getElementById(dugme).classList.remove("trenutno");
+    dugme = "autordugme";
+    document.getElementById("autordugme").classList.add("trenutno");
+})
+
+document.getElementById('uputstvodugme').addEventListener("click", ()=>{
+    document.getElementById(stranica).classList.add("skriveno")
+    stranica = "uputstvo";
+    document.getElementById('uputstvo').classList.remove("skriveno");
+
+    document.getElementById(dugme).classList.remove("trenutno");
+    dugme = "uputstvodugme";
+    document.getElementById('uputstvodugme').classList.add("trenutno");
+})
