73686ed640533db6c2f27d96f2ada693236241fe
Jovan Kostic <kosticjovan1@gmail.com>
2022-12-15 12:01:54 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+z1 = new Audio()
+z2 = new Audio()
+z3 = new Audio()
+z4 = new Audio()
+z5 = new Audio()
+
+
+document.getElementById('z1').addEventListener("mouseover", ()=>{
+    z1.play();
+})
+document.getElementById('z2').addEventListener("mouseover", ()=>{
+    z2.play();
+})
+document.getElementById('z3').addEventListener("mouseover", ()=>{
+    z3.play();
+})
+document.getElementById('z4').addEventListener("mouseover", ()=>{
+    z4.play();
+})
+document.getElementById('z5').addEventListener("mouseover", ()=>{
+    z5.play();
+})
+
